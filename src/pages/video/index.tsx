c7bac0b814c066a8d347baf542a809ba5f16c684
Your Name <casey.vukelich@outlook.com>
2025-04-04 12:20:19 -0500

    Add typescript notation

--- src/pages/video/index.tsx
+++ src/pages/video/index.tsx
@@ -13,112 +13,112 @@ recall i have vid streaming working:
 */
 
 export default function Home() {
-  const video = useRef(null)
-  const bufferedProgressAmount = useRef(null)
-  const playPause = useRef(null)
-  const stop = useRef(null)
-  const mute = useRef(null)
-  const volInc = useRef(null)
-  const volDec = useRef(null)
-  const progress = useRef(null)
-  const fullscreen = useRef(null)
-  const bufferedAmount = useRef(null)
+  const video = useRef<HTMLVideoElement>(null)
+  const bufferedProgressAmount = useRef<HTMLSpanElement>(null)
+  const playPause = useRef<HTMLButtonElement>(null)
+  const stop = useRef<HTMLButtonElement>(null)
+  const mute = useRef<HTMLButtonElement>(null)
+  const volInc = useRef<HTMLButtonElement>(null)
+  const volDec = useRef<HTMLButtonElement>(null)
+  const progress = useRef<HTMLProgressElement>(null)
+  const fullscreen = useRef<HTMLButtonElement>(null)
+  const bufferedAmount = useRef<HTMLSpanElement>(null)
 
   // setup custom video controls
-  const videoControls = useRef(null)
-  const videoContainer = useRef(null)
+  const videoControls = useRef<HTMLUListElement>(null)
+  const videoContainer = useRef<HTMLElement>(null)
 
   useEffect(() => {
     // check if the browser supports <video>
-    if (video.current.canPlayType) {
+    if (video.current!.canPlayType) {
       // disable browser's default controls
-      video.current.controls = false
+      video.current!.controls = false
 
       // display user defined video controls
-      videoControls.current.style.display = 'block'
+      videoControls.current!.style.display = 'block'
 
       // hide fullscreen buttoon if fullscreen api not enabled
-      if (!document?.fullscreenEnabled) fullscreen.current.style.display = 'none'
+      if (!document?.fullscreenEnabled) fullscreen.current!.style.display = 'none'
 
-      function alterVolume(dir) {
-        const currentVolume = Math.floor(video.current.volume * 10) / 10
+      function alterVolume(dir: string) {
+        const currentVolume = Math.floor(video.current!.volume * 10) / 10
 
-        if (dir === '+' && currentVolume < 1) video.current.volume += 0.1
-        else if (dir === '-' && currentVolume > 0) video.current.volume -= 0.1
+        if (dir === '+' && currentVolume < 1) video.current!.volume += 0.1
+        else if (dir === '-' && currentVolume > 0) video.current!.volume -= 0.1
       }
 
-      function setFullscreenData(state) {
-        videoContainer.current.setAttribute('data-fullscreen', !!state)
+      function setFullscreenData(state: Boolean) {
+        videoContainer.current!.setAttribute('data-fullscreen', (!!state).toString()) // TODO-i dont want to remove !! until its in its own commit
       }
 
-      playPause.current.addEventListener('click', e => {
-        if (video.current.paused || video.current.ended) video.current.play()
-        else video.current.pause()
+      playPause.current!.addEventListener('click', e => {
+        if (video.current!.paused || video.current!.ended) video.current!.play()
+        else video.current!.pause()
       })
 
-      stop.current.addEventListener('click', e => {
-        video.current.pause()
-        video.current.currentTime = 0
-        progress.current.value = 0
+      stop.current!.addEventListener('click', e => {
+        video.current!.pause()
+        video.current!.currentTime = 0
+        progress.current!.value = 0
       })
 
-      mute.current.addEventListener('click', e => {
-        video.current.muted = !video.current.muted
+      mute.current!.addEventListener('click', e => {
+        video.current!.muted = !video.current!.muted
       })
 
-      volInc.current.addEventListener('click', e => {
+      volInc.current!.addEventListener('click', e => {
         alterVolume('+')
       })
 
-      volDec.current.addEventListener('click', e => {
+      volDec.current!.addEventListener('click', e => {
         alterVolume('-')
       })
 
-      video.current.addEventListener('loadedmetadata', () => {
+      video.current!.addEventListener('loadedmetadata', () => {
         // set the progress bar's maximum value (length of video)
         // duration available for read on loadedmetadata event
-        progress.current.setAttribute('max', video.current.duration)
+        progress.current!.setAttribute('max', video.current!.duration.toString())
       })
 
-      video.current.addEventListener('progress', () => {
-        const duration = video.current.duration
+      video.current!.addEventListener('progress', () => {
+        const duration = video.current!.duration
 
         if (duration > 0) {
-          for (let i = 0; i < video.current.buffered.length; i++) {
-            if (video.current.buffered.start(video.current.buffered.length - 1 - i) < video.current.currentTime) {
-              bufferedAmount.current.style.width = `${(video.current.buffered.end(video.current.buffered.length - 1 - i) * 100) / duration}%`
+          for (let i = 0; i < video.current!.buffered.length; i++) {
+            if (video.current!.buffered.start(video.current!.buffered.length - 1 - i) < video.current!.currentTime) {
+              bufferedAmount.current!.style.width = `${(video.current!.buffered.end(video.current!.buffered.length - 1 - i) * 100) / duration}%`
               break;
             }
           }
         }
       })
 
-      video.current.addEventListener("timeupdate", () => {
+      video.current!.addEventListener("timeupdate", () => {
         // some mobile browsers don't update loadedmatadata like desktop browsers
         // video duration set at this point in most mobile browsers
-        if (!progress.current.getAttribute('max')) progress.current.setAttribute('max', video.current.duration)
+        if (!progress.current!.getAttribute('max')) progress.current!.setAttribute('max', video.current!.duration.toString())
 
-        progress.current.value = video.current.currentTime;
+        progress.current!.value = video.current!.currentTime;
 
         // TODO-will need to add this feature to the reagular video progress bar. 'media buffering, seeking and ranges has jsbin.com code to see examples
         // progress bar on available to play
-        if (video.current.duration > 0)
-          bufferedProgressAmount.current.style.width = `${(video.current.currentTime / video.current.duration) * 100}%`
+        if (video.current!.duration > 0)
+          bufferedProgressAmount.current!.style.width = `${(video.current!.currentTime / video.current!.duration) * 100}%`
       })
 
       // Add skip ahead
-      progress.current.addEventListener('click', e => {
-        const rect = progress.current.getBoundingClientRect()
-        const pos = (e.pageX - rect.left) / progress.current.offsetWidth
-        video.current.currentTime = pos * video.current.duration
+      progress.current!.addEventListener('click', e => {
+        const rect = progress.current!.getBoundingClientRect()
+        const pos = (e.pageX - rect.left) / progress.current!.offsetWidth
+        video.current!.currentTime = pos * video.current!.duration
       })
 
-      fullscreen.current.addEventListener('click', e => {
+      fullscreen.current!.addEventListener('click', e => {
         if (document.fullscreenElement !== null) { // in fullscreen mode
           document.exitFullscreen()
           setFullscreenData(false)
         } else {
-          videoContainer.current.requestFullscreen()
+          videoContainer.current!.requestFullscreen()
           setFullscreenData(true)
         }
       })
@@ -145,15 +145,15 @@ export default function Home() {
         have other fallbacks w/ different media types of video, ex: .ogg
         fallback:<a href="https://archive.org/details/BigBuckBunny_124">here</a>.
       </video>
-      <ul id="video-controls" ref={videoControls} class="controls">
-        <li class="progress">
+      <ul id="video-controls" ref={videoControls} className="controls">
+        <li className="progress">
           <progress id="progress" ref={progress} value="0" min="0" />
         </li>
         <li>
-          <div class="buffered">
+          <div className="buffered">
             <span id="buffered-amount" ref={bufferedAmount} ></span>
           </div>
-          <div class="buffered-progress">
+          <div className="buffered-progress">
             <span id="buffered-progress-amount" ref={bufferedProgressAmount}></span>
           </div>
         </li>
